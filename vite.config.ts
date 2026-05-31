b731d2f266fbc64725a07ea957bc6304b3038c02
ryankimutaikutto-code <ryankimutaikutto@gmail.com>
2026-05-31 12:35:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/'
-})
+  base: '/my-portfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
